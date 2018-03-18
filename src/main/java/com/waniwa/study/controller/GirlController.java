9501944edc7cfde4fd667bdc29fd82351c740579
wanniwa <734933034@qq.com>
2018-03-19 01:00:08 +0800

    更新

--- src/main/java/com/waniwa/study/controller/GirlController.java
+++ src/main/java/com/waniwa/study/controller/GirlController.java
@@ -5,6 +5,8 @@ import com.waniwa.study.domain.Result;
 import com.waniwa.study.repository.GirlRepository;
 import com.waniwa.study.service.GirlService;
 import com.waniwa.study.utils.ResultUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -13,57 +15,80 @@ import javax.validation.Valid;
 import java.util.List;
 
 /**
- * @author wanniwa
- * @date 2018/3/17 23:49
+ * Created by 廖师兄
+ * 2016-11-03 23:15
  */
 @RestController
 public class GirlController {
+
+    private final static Logger logger = LoggerFactory.getLogger(GirlController.class);
+
     @Autowired
     private GirlRepository girlRepository;
+
     @Autowired
     private GirlService girlService;
+
     /**
-     * 查询所有女生
-     *
+     * 查询所有女生列表
      * @return
      */
     @GetMapping(value = "/girls")
     public List<Girl> girlList() {
+        logger.info("girlList");
+
         return girlRepository.findAll();
     }
 
     /**
      * 添加一个女生
-     * @param girl
-     * @param
      * @return
      */
     @PostMapping(value = "/girls")
     public Result<Girl> girlAdd(@Valid Girl girl, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return ResultUtil.error(1,bindingResult.getFieldError().getDefaultMessage());
+            return ResultUtil.error(1, bindingResult.getFieldError().getDefaultMessage());
         }
+
+        girl.setCupSize(girl.getCupSize());
+        girl.setAge(girl.getAge());
 
         return ResultUtil.success(girlRepository.save(girl));
     }
 
-    @PostMapping(value = "/girls/{id}")
-    public Girl girlAdd(@PathVariable("id") Integer id,
-                        @RequestParam("cupSize") String cupSize,
-                        @RequestParam("age") Integer age) {
+    //查询一个女生
+    @GetMapping(value = "/girls/{id}")
+    public Girl girlFindOne(@PathVariable("id") Integer id) {
+        return girlRepository.findOne(id);
+    }
+
+    //更新
+    @PutMapping(value = "/girls/{id}")
+    public Girl girlUpdate(@PathVariable("id") Integer id,
+                           @RequestParam("cupSize") String cupSize,
+                           @RequestParam("age") Integer age) {
         Girl girl = new Girl();
         girl.setId(id);
         girl.setCupSize(cupSize);
         girl.setAge(age);
+
         return girlRepository.save(girl);
     }
 
-    @GetMapping(value = "/girls/{age}")
-    public List<Girl> girlAdd(@PathVariable("age") Integer age) {
+    //删除
+    @DeleteMapping(value = "/girls/{id}")
+    public void girlDelete(@PathVariable("id") Integer id) {
+        girlRepository.delete(id);
+    }
+
+    //通过年龄查询女生列表
+    @GetMapping(value = "/girls/age/{age}")
+    public List<Girl> girlListByAge(@PathVariable("age") Integer age) {
         return girlRepository.findByAge(age);
     }
+
     @PostMapping(value = "/girls/two")
-    public void girlTwo(){
+    public void girlTwo() {
         girlService.insertTwo();
     }
 
